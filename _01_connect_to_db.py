083f4af9df84a6c7b1e8fe0942e04597e5995969
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-08-24 12:55:45 +0600

    connetion with heroku postgres database and added confidential variable values to config vars of heroku

--- _01_connect_to_db.py
+++ _01_connect_to_db.py
@@ -3,15 +3,16 @@ from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_sqlalchemy import SQLAlchemy
 from flask_gravatar import Gravatar
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/database/blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
