e87fd51202a17b833650cc54fc146908a44c1397
ITS MAC <asifchowdhuryrafi143@outlook.com>
2021-08-24 19:36:43 +0600

    postgres database support with SQLAlchemy

--- _01_connect_to_db.py
+++ _01_connect_to_db.py
@@ -13,7 +13,12 @@ Bootstrap(app)
 
 ##CONNECT TO DB
 # I need the local database to develop the app locally
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///static/database/blog.db')
+URI = os.environ.get('DATABASE_URL','sqlite:///static/database/blog.db')
+# SQLAlchemy 1.4.x has removed support for the postgres:// URI scheme, which is used by Heroku Postgres. To maintain compatibility, I have to use this lines of code
+if URI.startswith("postgres://"):
+    URI = URI.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
